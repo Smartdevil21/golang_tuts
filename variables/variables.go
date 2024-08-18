6df09e5acbd21578c7992e43ebae374f7a8c679d
Smartdevil21 <prtk.app.dev@gmail.com>
2024-08-18 13:59:43 +0530

    Variables

---
+++ variables/variables.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main(){
+	var name string = "Pratik";
+	var num float32 = 12.22;
+
+	surname := "Vaidya";
+	
+	fmt.Println(name)
+	fmt.Println( num)
+	fmt.Println( surname)
+}
